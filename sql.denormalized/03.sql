dce56f1b023dc7e6de377d2fedcd3b005f11ea2e
April Zhao <abooli@outlook.com>
2022-04-22 12:30:32 -0700

    ?

--- sql.denormalized/03.sql
+++ sql.denormalized/03.sql
@@ -14,3 +14,13 @@
 /*
 SELECT lower(data->'place'->>'country_code') AS country_code, count(*) as count
 Fxx*/
+SELECT data->>'lang' AS lang, count(*) as count
+FROM (SELECT data ->> 'id' AS id,
+    '#' || (jsonb_array_elements(COALESCE(data->'entities'->'hashtags','[]') ||
+            COALESCE(data->'extended_tweet'->'entities'->'hashtags','[]') AS tag
+            FROM tweets_jsonb
+            WHERE data->'entities'->'hashtags' @@ '$[*].text=="coronavirus"'
+            OR data->'extended_tweet'->'entities'->'hashtags' @@ '$[*].text=="coronavirus" '
+        )t
+GROUP BY lang
+ORDER BY count DESC, lang;
--- sql.denormalized/05.sql
+++ sql.denormalized/05.sql
@@ -18,4 +18,14 @@
 --ORDER BY count DESC,tag
 --LIMIT 1000
 --;
-
+SELECT tag, count(*) AS count
+FROM (SELECT data->>'id' AS id, '#' || (jsonb_array_elements(COALESCE(data->'entities'->'hashtags','[]') || 
+            COALESCE(data->'extended_tweet'->'entities'->'hashtags','[]'))->>'text') AS tag
+    FROM tweets_jsonb
+    WHERE (to_tsvector('english', data->'extended_tweet'->'full_text') @@ to_tsquery('english', 'coronavirus')
+   OR to_tsvector('english', data->'text') @@ to_tsquery('english', 'coronavirus'))
+    AND data->>'lang'='en'
+)t
+GROUP BY tag
+ORDER BY count DESC, tag
+LIMIT 10000
